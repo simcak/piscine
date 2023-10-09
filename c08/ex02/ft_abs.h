7ddf5647e7b1e5e0f5ad1752e25443451f117521
simcak <peta.simcak@gmail.com>
2023-10-09 19:01:43 +0200

    correct one

---
+++ c08/ex02/ft_abs.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_abs.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 14:32:48 by peta              #+#    #+#             */
+/*   Updated: 2023/06/29 15:46:12 by psimcak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_ABS_H
+# define FT_ABS_H
+
+# define ABS(Value) ((Value < 0) ? -Value : Value)
+
+#endif
+
+// ifdef && ifndef definují podmínku, že header buď byl a nebo nebyl definován
+// --> if defined
+// --> if not defined
---
+++ c08/ex03/ft_point.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/25 14:48:34 by peta              #+#    #+#             */
+/*   Updated: 2023/06/29 15:46:31 by psimcak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+#endif
+
+// struktura: typedef old_name new_name {obsah}
